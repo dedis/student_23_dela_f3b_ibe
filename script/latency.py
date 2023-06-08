e0f5575175875d52a951f4d5a0860a2aa53ac149
Louis Bettens <louis@bettens.info>
2023-06-08 11:55:10 +0200

    make into a comparison

--- script/latency.py
+++ script/latency.py
@@ -6,12 +6,12 @@ from numpy import genfromtxt
 from tool import *
 import csv
 
-data = read_datafile('./data/IBE_records.csv')
+data1 = read_datafile("data/IBE_records.csv")
+data2 = read_datafile("data/latency.csv")
 
-x= data[:,0]
-y1= data[:,1]
-y2= data[:,2]
-y3= data[:,3]
+x= data1[:,0]
+y1= data1[:,1]
+y2= data2[:,1]
 
 
 ind = np.arange(len(x))  # the x locations for the groups
@@ -19,8 +19,7 @@ width = 0.3      # the width of the bars
 
 fig, ax = plt.subplots()
 rects1 = ax.plot(ind + width, y1, color=red[0], marker=".")
-rects2 = ax.bar(ind + width, y2, width, color=purple[1], edgecolor=purple[0])
-rects3 = ax.bar(ind + 2*width, y3, width, color=yellow[1], edgecolor=yellow[0])
+rects2 = ax.plot(ind + width, y2, color=purple[0], marker=".")
 
 # add some text for labels, title and axes ticks
 ax.set_yscale('log')
@@ -35,6 +34,6 @@ ax.yaxis.set_major_formatter(plt.FuncFormatter(format_func))
 for label in (ax.get_xticklabels() + ax.get_yticklabels()):
     label.set_fontsize(0.8*fs_axis)
 
-ax.legend((rects1[0], rects2[0],rects3[0]), ('DKG Setup','Write Transaction','Key Reconstruction'), loc=2,fontsize=0.8*fs_label)
+ax.legend((rects1[0], rects2[0]), ('IBE','TDH2'), loc=2,fontsize=0.8*fs_label)
 plt.tight_layout()
 save_pdf("IBE_latency.pdf")
